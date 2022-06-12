dadffb62ff4a7987adb62eb7a89ee09878af2569
gen0x39 <migesan0319@gmail.com>
2022-06-12 21:35:52 +0900

    Modyfy: src/backend/bot.js

--- src/backend/bot.js
+++ src/backend/bot.js
@@ -1,3 +1,5 @@
+// https://scrapbox.io/discordjs-japan/%E3%82%B9%E3%83%A9%E3%83%83%E3%82%B7%E3%83%A5%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%88%E3%81%86
+
 const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 const Commands = require('./commands.js');
@@ -13,35 +15,41 @@ client.once("ready", async () => {
     console.log("Ready!");
 });
 
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) {
-        return;
-    }
-    if (interaction.commandName === 'ping') {
+const commands = {
+    async ping(interaction) {
         await interaction.reply('Pong!');
-    }
-    else if (interaction.commandName === 'tl') {
+        return;
+    },
+    async tl(interaction) {
         await interaction.deferReply();
         const options = {
             mode: "json",
             pythonPath: "/usr/bin/python3",
             pythonOptions: ["-u"],
             scriptPath: "/workspace/src/script",
-          };
-        var shell = new PythonShell("script.py", options);
-        console.log(interaction.options.getString('input'))
+        };
+        const shell = new PythonShell("translate.py", options);
         const json = {
-            "name": "tachiba7",
-            "text": interaction.options.getString('input')
+            "text": interaction.options.getString("input")
         };
         shell.send(json);
-        shell.on('message', function (data) {
-            console.log(JSON.stringify(data, null, 2));
-            console.log(data.result.JP)
+        shell.on("message", function (data) {
             interaction.editReply(data.result.JP);
         });
+        return;
     }
-});
+};
+
+async function onInteraction(interaction) {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    return commands[interaction.commandName](interaction);
+}
+client.on("interactionCreate", interaction => onInteraction(interaction).catch(err => console.error(err)));
 
 token = process.env.APIKEY
-client.login(token);
+client.login(token).catch(err => {
+    console.log(err);
+    process.exit(-1);
+});
