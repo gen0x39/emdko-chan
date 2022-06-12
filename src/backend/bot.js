19e7837162fc47a70b074bfbd870e217704c202f
gen0x39 <migesan0319@gmail.com>
2022-06-12 22:35:25 +0900

    Fix: remove unused val

--- src/backend/bot.js
+++ src/backend/bot.js
@@ -6,26 +6,6 @@ const Commands = require('./commands.js');
 const { PythonShell } = require('python-shell');
 const { MessageEmbed } = require('discord.js');
 
-// inside a command, event listener, etc.
-const exampleEmbed = new MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/AfFp7pu.png')
-	.setTimestamp()
-	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-
 dotenv.config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
